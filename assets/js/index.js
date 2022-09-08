5897dc2d5122c0cf1d63f2847be204df0d543d5b
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-09-08 13:37:49 +0200

    added example code from airtable

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'YOUR_API_KEY'}).base('app71fe0Ff06gsUXD');
+
+base('structures').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('name'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
