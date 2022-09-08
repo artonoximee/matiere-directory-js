ee35d182b41f56efdc7a61275a9ad18ec06d2092
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-09-08 14:51:27 +0200

    added selector in html, started querying function in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,20 @@
-fetch("https://api.airtable.com/v0/app71fe0Ff06gsUXD/structures", {headers: { Authorization: 'Bearer keyEgsODRGeMoFEqh' }})
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    document.getElementById("test").innerHTML = value.records[0].fields.name;
-    console.log(value);
-  })
-  .catch(function(err) {
-    // Une erreur est survenue
-  });
+function getStructures() {
+  var selectedDepartment = document.getElementById('department').value;
+  // document.getElementById("test").innerHTML = selectedDepartment;
+  fetchStructures(selectedDepartment);
+}
+
+function fetchStructures(selectedDepartment) {
+  fetch("https://api.airtable.com/v0/app71fe0Ff06gsUXD/structures", {headers: { Authorization: 'Bearer keyEgsODRGeMoFEqh' }})
+    .then(function(res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function(value) {
+      document.getElementById("test").innerHTML = value.records[0].fields.postcode;
+      console.log(value);
+    })
+    .catch(function(err) {
+    });
+}
