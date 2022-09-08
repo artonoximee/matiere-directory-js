8835e3669e1e003a6c60d8a0b4974900d79f532f
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-09-08 14:24:24 +0200

    resolved accessing to airtable api

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,13 @@
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'YOUR_API_KEY'}).base('app71fe0Ff06gsUXD');
-
-base('structures').select({
-    // Selecting the first 3 records in Grid view:
-    maxRecords: 3,
-    view: "Grid view"
-}).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('name'));
-    });
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
+fetch("https://api.airtable.com/v0/app71fe0Ff06gsUXD/structures", {headers: { Authorization: 'Bearer keyEgsODRGeMoFEqh' }})
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    document.getElementById("test").innerHTML = value;
+    console.log(value);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
